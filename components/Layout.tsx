5dd9e0b5ab2968e71803799a96428bcb95ec4f46
Jeffrey Canty <jeffrey@tremendous.dev>
2022-05-08 22:57:20 -0500

    WIP

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,32 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
 
-const Root = styled.div`
-  padding: ${(props) => props.theme.space[3]}px;
-
-  & h1 {
-    color: ${(props) => props.theme.colors.black};
-  }
-
-  & p {
-    color: ${(props) => props.theme.colors.gray[900]};
-  }
+const Root = styled.section`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
 `;
 
-interface Props {
-  children?: ReactNode;
+const Header = styled.header``;
+
+const Content = styled.main`
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+`;
+
+const Footer = styled.footer``;
+
+interface LayoutProps {
+  children: ReactNode;
 }
 
-export const Layout = ({ children }: Props) => <Root>{children}</Root>;
+export const Layout = ({ children }: LayoutProps) => <Root>{children}</Root>;
+
+Layout.Header = Header;
+Layout.Content = Content;
+Layout.Footer = Footer;
+
+Layout.displayName = "Layout";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,123 @@
+import styled from "styled-components";
 import { Layout } from "@components";
+
+import {
+  AnimatePresence,
+  motion,
+  HTMLMotionProps,
+  Variants,
+} from "framer-motion";
+
+const headerVariants: Variants = {
+  out: { y: "-100%" },
+  in: {
+    y: 0,
+    transition: {
+      bounce: 0.125,
+      type: "spring",
+      duration: 0.5,
+    },
+  },
+};
+const Header = styled(motion.div)<HTMLMotionProps<"div">>`
+  background-color: red;
+  padding: 2rem;
+`;
+
+const footerVariants: Variants = {
+  out: { y: "100%" },
+  in: {
+    y: 0,
+    transition: {
+      bounce: 0.125,
+      type: "spring",
+      duration: 0.5,
+    },
+  },
+};
+
+const Footer = styled(motion.div)<HTMLMotionProps<"div">>`
+  background-color: green;
+  padding: 1rem 2rem;
+`;
+
+const Root = styled(motion.div)<HTMLMotionProps<"div">>`
+  background-color: purple;
+  width: 100%;
+  height: 100%;
+  border: 1px solid red;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  max-width: 960px;
+  margin: 0 auto;
+
+  & h1 {
+    margin-bottom: 1rem;
+  }
+
+  & h1,
+  & p {
+    max-width: 54ch;
+    border: 1px solid red;
+  }
+`;
+
+const rootVariants: Variants = {
+  out: { opacity: 0 },
+  in: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.25,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const childVariants: Variants = {
+  out: { opacity: 0, y: "25%" },
+  in: { opacity: 1, y: 0 },
+};
+
+const Content = () => (
+  <Root variants={rootVariants} initial="out" animate="in" exit="out">
+    <motion.h1 variants={childVariants} layout>
+      Retro brunch quinoa williamsburg actually austin VHS meh. Echo park health
+      goth viral truffaut.
+    </motion.h1>
+    <motion.p variants={childVariants} layout>
+      Readymade chicharrones viral tilde hoodie bitters street art thundercats
+      tofu chillwave DSA keffiyeh narwhal church-key helvetica. Hella try-hard
+      before they sold out sriracha, mustache hoodie heirloom. Leggings franzen
+      whatever gluten-free green juice. Wolf poutine you probably haven't heard
+      of them pork belly activated charcoal umami hella biodiesel, kombucha
+      gentrify narwhal 3 wolf moon before they sold out. PBR&B next level
+      succulents cliche tumeric readymade.
+    </motion.p>
+  </Root>
+);
 
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>Application</h1>
+      <AnimatePresence>
+        <Header variants={headerVariants} initial="out" animate="in" exit="out">
+          <Layout.Header>
+            <h3>Header</h3>
+          </Layout.Header>
+        </Header>
+        <Layout.Content>
+          <Content />
+        </Layout.Content>
+        <Footer variants={footerVariants} initial="out" animate="in" exit="out">
+          <Layout.Footer>
+            <h3>Footer</h3>
+          </Layout.Footer>
+        </Footer>
+      </AnimatePresence>
     </Layout>
   );
 };
